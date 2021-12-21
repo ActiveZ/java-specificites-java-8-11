3cea7b018649215abbe7164241b0b19603041ee0
ActiveZ <activez.git@gmail.com>
2021-12-21 12:01:04 +0100

    test 2 ok

--- src/test/java/chap_01_methodes_par_defaut/ex02/Method_02_Test.java
+++ src/test/java/chap_01_methodes_par_defaut/ex02/Method_02_Test.java
@@ -18,6 +18,9 @@ public class Method_02_Test {
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
         // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        default String format() {
+            return "[" + findAll().size() + " persons]";
+        }
     }
 
     class DaoA implements IDao {
@@ -34,6 +37,10 @@ public class Method_02_Test {
         // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
         // TODO l'implémentation réutilise la méthode format() de l'interface
 
+        @Override
+        public String format() {
+            return "DaoA" + IDao.super.format();
+        }
     }
 
     @Test
@@ -42,8 +49,7 @@ public class Method_02_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
-
+        String result = daoA.format();
         assert "DaoA[20 persons]".equals(result);
     }
 }
